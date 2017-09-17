e717e920cc4a97365be069c97c434b71d743676a
Group5SE <31965266+Group5SE@users.noreply.github.com>
2017-09-17 14:31:32 -0500

    Create Clause.h

---
+++ src/iProlog/Clause.h
@@ -0,0 +1,36 @@
+/*
+Author: Karthik Venkataramana Pemmaraju
+Date: 09/17/2017
+Description: Data structure representation of a prolog Clause.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+namespace iProlog {
+/*
+    @type: class
+    @desc: Provides a template for a prolog clause.
+*/
+
+  class Clause {
+    private:
+      const int len; // length of heap slice
+      const vector<int> hgs; // head+goals pointing to cells in cs
+      const int base; // heap where this starts
+      const int neck; // first after the end of the head
+      const vector<int> xs; // indexables in head
+    public:
+      Clause(int l, vector<int> h, int b, int n, vector<int> x); // Parameterized constructor.
+  };
+
+  // Parameterized constructor to instantiate const variables.
+  Clause::Clause(int l, vector<int> h, int b, int n, vector<int> x)
+    : len(l)
+    , hgs(h)
+    , base(b)
+    , neck(n)
+    , xs(x){ // Default constructor for const variables.
+  }
+}
