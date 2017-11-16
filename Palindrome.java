0cac13db8ade79b79bb43773778e0f0ee17a49b6
karthikVenkataramana <30518687+karthikVenkataramana@users.noreply.github.com>
2017-11-16 10:52:14 -0600

    Added output

--- Palindrome.java
+++ Palindrome.java
@@ -9,7 +9,7 @@ level
 
 /*
 
-
+GIT LINK: https://github.com/Group5SE/PrologVM/tree/exam 
 E:\Software Engineering\Exam_Karthik>git remote add origin https://github.com/Group5SE/PrologVM.git
 E:\Software Engineering\Exam_Karthik>git add words.txt
 E:\Software Engineering\Exam_Karthik>git add Palindrome.java
@@ -29,7 +29,33 @@ Total 4 (delta 0), reused 0 (delta 0)
 To https://github.com/Group5SE/PrologVM.git
  * [new branch]      exam -> exam
 
+E:\Software Engineering\Exam_Karthik>git commit -a -m "Added git commands"
+[exam 28befa0] Added git commands
+ 1 file changed, 25 insertions(+)
 
+E:\Software Engineering\Exam_Karthik>git push origin exam -f
+Counting objects: 3, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (3/3), 738 bytes | 0 bytes/s, done.
+Total 3 (delta 1), reused 0 (delta 0)
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/Group5SE/PrologVM.git
+   f914b35..28befa0  exam -> exam
+
+E:\Software Engineering\Exam_Karthik>git commit -a -m "Added output"
+[exam 09d1cb4] Added output
+ 1 file changed, 9 insertions(+)
+
+E:\Software Engineering\Exam_Karthik>git push origin exam -f
+Counting objects: 3, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (3/3), 365 bytes | 0 bytes/s, done.
+Total 3 (delta 1), reused 0 (delta 0)
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/Group5SE/PrologVM.git
+   28befa0..09d1cb4  exam -> exam
  */
 
 import java.util.*;
@@ -37,12 +63,12 @@ import java.io.*;
 class Palindrome
 {
     public static boolean isPalindrome(String str) {
-        return str.equals(new StringBuilder(str).reverse().toString());
+        return str.equals(new StringBuilder(str).reverse().toString()); // Check if reverse is the original string.
     }
     public static void main(String[] args) throws FileNotFoundException, IOException{
         String everything;
         ArrayList<String> words = new ArrayList<String>();
-        BufferedReader br = new BufferedReader(new FileReader("words.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("words.txt")); // Read the list of words from file.
         try { 
             String line = br.readLine();
             while (line != null) { 
@@ -52,15 +78,15 @@ class Palindrome
             } 
         } 
         catch (FileNotFoundException ex)  
-        {
-            // insert code to run when exception occurs
+        { 
+            ; // Assuming file is found. Ha- ha !
         }
         finally {
             br.close();
         }  
         Iterator itr= words.iterator();  
         while(itr.hasNext()){  
-         System.out.println(itr.next());  
+         System.out.println(itr.next());   // Print only  words which are palindromes.
         }  
     }
 }
