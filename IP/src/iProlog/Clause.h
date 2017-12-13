32d9923d19124f994f1593ae35de5bb583b98972
charishma-d <31521269+charishma-d@users.noreply.github.com>
2017-12-13 16:56:46 -0600

    Update Clause.h

--- IP/src/iProlog/Clause.h
+++ IP/src/iProlog/Clause.h
@@ -1,6 +1,6 @@
 /*
 ==================================================================
-Author: Karthik Venkataramana Pemmaraju
+Author: Karthik Venkataramana Pemmaraju,Charishma Damuluri
 Description: Data structure representation of a prolog Clause.
 Compilation: g++ Clause.h
 Compiled and tested on  09/17/2017
@@ -9,10 +9,8 @@ Compiled and tested on  09/17/2017
 
 #ifndef CLAUSE
 #define CLAUSE
-
 #include <iostream>
 #include <vector> 
-
 namespace iProlog {
 /*
     @type: class
@@ -23,14 +21,13 @@ namespace iProlog {
     public:
       const int len; // length of heap slice
       const std::vector<int> hgs; // head+goals pointing to cells in cs
-      const int base; // heap where this starts
-      const int neck; // first after the end of the head
+      //neck-first after the end of head
+      const int neck; 
+      const int base;// heap where this starts 
       const std::vector<int> xs; // indexables in head
-    public:
       Clause(int l, std::vector<int> h, int b, int n, std::vector<int> x); // Parameterized constructor.
       template<typename T>
       static void printVector(std::vector<T> x);
   };
-
 }
 #endif
